018a243b32183f55660e4135a82607f66961beda
banch0u <fikret.okis@gmail.com>
2023-03-22 23:12:19 +0400

    tailwind instalation process

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
@@ -10,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <h1>Hello</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </div>
     </>
   );
