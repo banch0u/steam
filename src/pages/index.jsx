108305dffd433203f2789df94b9f9a1b216d84d4
banch0u <fikret.okis@gmail.com>
2023-03-22 23:08:00 +0400

    next default template cleanup

---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Steam</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+}
